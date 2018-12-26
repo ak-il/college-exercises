bf48684b9bc8bce93c6edf642358acfc13cc0312
Alex Kubica <alexkubicail@gmail.com>
2018-12-26 17:27:54 +0200

    add

--- cyber-a/ex2/simulation.py
+++ cyber-a/ex2/simulation.py
@@ -8,12 +8,18 @@ def print_usage(script_name):
     print('%s a b c' % script_name)
 
 def open_worm_table():
+    table = {}
     with open('./worm-table.csv', 'rb') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            print(', '.join(row))
+            table[row[0]] = {
+                'is_reachable': row[1] == 'Y',
+                'is_vulnerable': row[2] == 'Y',
+                'is_infected': row[3] == 'Y'
+            }
+    return table
 
-def start_simulation(a, b, c):
+def start_simulation(table, a, b, c):
     pass
 
 def main(script_name, argv):
@@ -21,9 +27,10 @@ def main(script_name, argv):
         a = int(argv[0])
         b = int(argv[1])
         c = int(argv[2])
-        open_worm_table()
-        start_simulation(a, b, c)
-    except:
+        table = open_worm_table()
+        start_simulation(table, a, b, c)
+    except Exception as e:
+        print(e)
         print_usage(script_name)
         sys.exit(2)
 
